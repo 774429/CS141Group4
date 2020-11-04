f25c432e62b1db6ddff3f618c5e495e7eacf53a8
774429 <Kgilliam@mail.greenriver.edu>
2020-11-04 11:41:38 -0800

    Create Hours.java
    added imports, class, main, scanner named input.

---
+++ Hours.java
@@ -0,0 +1,23 @@
+import java.io.FileNotFoundException;  // Import this class to handle errors
+import java.util.Scanner; // Import the Scanner class to read text files
+import java.util.ArrayList;// List
+import java.io.FileWriter;   // Import the FileWriter class
+import java.util.Random;// random numbers
+import java.io.IOException; // Handle exceptions
+import java.util.*;
+import java.util.List; // lists
+import java.util.Arrays;// arrays
+
+public class Hours{
+
+    
+
+    public static void main (String[]  args){
+    Scanner input = new Scanner (System.in);
+    char[] cArr =  input.toCharArray();
+    
+
+      
+   }
+
+}
